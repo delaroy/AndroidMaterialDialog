a5052bc2fab1a6b023cfab3f1b5404fe1251aa7a
Delaroy Studios <bamoyk@yahoo.com>
2016-09-05 14:57:16 -0700

    first commit

---
+++ app/src/main/java/com/delaroystudios/materialdialogs/PreferenceActivityCompat.java
@@ -0,0 +1,30 @@
+package com.delaroystudios.materialdialogs;
+
+/**
+ * Created by delaroystudios on 8/31/2016.
+ */
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+@TargetApi(Build.VERSION_CODES.GINGERBREAD_MR1)
+public class PreferenceActivityCompat extends android.preference.PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
